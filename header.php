a2807c49325fab63d63530739acd510ffcf0b3a4
manjunath <kalburgimanjunath@gmail.com>
2013-06-08 13:51:06 +0530

    add

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="css/test1.css">
+	<style type="text/css">
+@media print{
+body {display:none;}
+}
+</style>
+	<script type="text/javascript" src="javascript/jquery-1.7.1.min.js"></script>
+	</head>
---
+++ index.php
@@ -0,0 +1,68 @@
+<?php include("header.php");?>
+
+<section id="home">
+<div class="row-fluid">
+	<div class="container">
+		<?php include("nav.php");?></div></div>
+			    <div id="myCarousel" class="carousel slide">
+					<!-- Carousel items --><br/><br/><br/><br/>
+					<div class="carousel-inner">
+					<div class="active item">
+					<!--<h2>Web Development</h2>-->
+					<img src="images/banner.jpg" width=100% height="300" />
+					<!--<div class="carousel-caption">The web has become a Everywhere aspect of everyday life. My Solution is a leading Web 2.0 web design, web development and online marketing firm, delivering highly-creative, brand-driven and results-focused web solutions to corporate clients.It is established firm for professional web design and web development, with constantly improving client management, executive-level reporting and an ongoing investment in innovation. </div>-->
+					</div>
+					
+				<!-- Carousel nav -->
+			
+				</div>
+				</div>
+<div class="row-fluid">
+	<div class="container">		  
+		
+			<div class="page-header"></div>
+		
+			<div class="row-fluid">
+			
+				<div class="span4">              
+					<img src="images/banners/images/mobile_application.jpg" alt="">
+					<div class="caption">
+					  <h3>RECENT NEWS1</h3><br/>
+					  <p>Exceptional Solutions for your Mobile Development needs.</p>
+					  <br/><br/><br/>
+					  <p><a href="#" class="btn btn-info">Know more</a></p>
+					</div>          
+				</div><!--/span-->
+			
+			
+			   <div class="span4">              
+					<img src="images/banners/images/online_sales.jpg" alt="">
+					<div class="caption">
+					  <h3>RECENT NEWS2</h3><br/>
+					  <p>Customized web development services are at your reach with us. Interactive Web 2.0 enabled websites with attractive design, simple navigation, and rich functionalities are not a dream any more.</p>
+					  <p><a href="#" class="btn btn-info">Know more</a></p>
+					</div>          
+				</div><!--/span-->
+			   <div class="span4">              
+					<img src="images/banners/images/web_presents.jpg" alt="">
+					<div class="caption">
+					  <h3>RECENT NEWS3</h3><br/>
+					  <p>Many well known, world class companies are famous for their excellence in managing their operations processes.We help them to achieve.</p>
+					  <br/><br/>
+					  <p><a href="#" class="btn btn-small btn-info">Know more</a></p>
+					</div>          
+				</div><!--/span-->
+				<div class="span12"><hr width=98%/></div> 
+				<div class="span12">
+					The web has become a Everywhere aspect of everyday life. My Solution is a leading Web 2.0 web design.
+					<img src="images/arrow.jpg"/>
+				
+					</div>
+				</div>
+				
+				
+				<div class="span12"></div> 
+				
+			</div><!--/row-->
+</section>
+<?php include("footer.php");?>
